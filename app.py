a803edd43b31dc7e7e8f7aea18cba20eef5e9d85
ramneekkaur2310-ui <ramneekkaur2310@gmail.com>
2026-07-28 12:51:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -128,12 +128,6 @@ if st.button('generate resume'):
         code = code.replace("PROFILE_IMAGE_PLACEHOLDER", data_uri)
 
     st.html(code , width="stretch" , unsafe_allow_javascript=True)
-      
-    response = agent.invoke({'messages': [{'role':'user','content':query}]})
-    print(response['messages'][-1].content)
-    code=response['messages'][-1].content[-1]['text']
-    #st.markdown(code)
-    st.html(code , width="stretch" , unsafe_allow_javascript=True)
 
 
 
