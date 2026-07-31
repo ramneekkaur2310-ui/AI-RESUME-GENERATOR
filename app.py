a0d43910c1e6b94a9731c14a484d81420bb21b0d
ramneekkaur2310-ui <ramneekkaur2310@gmail.com>
2026-07-31 12:33:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -151,6 +151,33 @@ if st.button('generate resume'):
     st.html(job_code, width='stretch', unsafe_allow_javascript=True)
 
 
+#================== STEP 2 ==========================#
+#To show web-app: complete page layout
+st.set_page_config(layout="wide")
+
+st.title("AI PPT GENERATOR")
+st.divider()
+st.sidebar.title("Enter API-KEYS")
+
+#====================== STEP 3 =========================#
+GOOGLE_API_KEY=st.sidebar.text_input("GOOGLE-API",type="password")
+TAVILY_API_KEY=st.sidebar.text_input("TAVILY-API",type="password")
+
+#===================== API VALIDATION ==================#
+ALL_API=[GOOGLE_API_KEY,TAVILY_API_KEY]
+
+if not all(ALL_API):
+    st.sidebar.error("MUST PASS ALL API-KEYS")
+
+elif all(ALL_API):
+    st.sidebar.success("API-KEYS LOADED SUCCESSFULLY")
+    #MODEL LOAD
+    model=ChatGoogleGnerativeAI(
+        google_api_key=GOOGLE_API_KEY,
+        model=st.sidebar.selectbox("
+
+
+
 
 
 
